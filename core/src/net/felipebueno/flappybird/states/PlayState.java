a9203d7ccbde34a9988712a2e4dfa5324a67d21e
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-07 17:09:56 -0300

    episode 7: make bird fly and add backgound and ground to the PlayState

--- core/src/net/felipebueno/flappybird/states/PlayState.java
+++ core/src/net/felipebueno/flappybird/states/PlayState.java
@@ -2,6 +2,7 @@ package net.felipebueno.flappybird.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import net.felipebueno.flappybird.sprites.Bird;
@@ -12,10 +13,14 @@ import static net.felipebueno.flappybird.FlappyBird.WIDTH;
 public class PlayState extends State {
 
 	private final Bird bird;
+	private final Texture bg;
+	private final Texture ground;
 
 	public PlayState(GameStateManager manager) {
 		super(manager);
 		bird = new Bird(50, 100);
+		bg = new Texture("bg.png");
+		ground = new Texture("ground.png");
 
 		camera.setToOrtho(false, WIDTH / 2, HEIGHT / 2);
 
@@ -25,6 +30,9 @@ public class PlayState extends State {
 	protected void handleInput() {
 		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
 			manager.set(new MenuState(manager));
+
+		if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.J))
+			bird.jump();
 	}
 
 	@Override
@@ -37,12 +45,16 @@ public class PlayState extends State {
 	public void render(SpriteBatch batch) {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(bg, camera.position.x - (camera.viewportWidth / 2), 0);
+		batch.draw(ground, camera.position.x - (camera.viewportWidth / 2), 0);
 		batch.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
+		bg.dispose();
+		ground.dispose();
 		bird.getTexture().dispose();
 	}
 }
