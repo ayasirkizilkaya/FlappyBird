9b5ea412eb6766508a4507ba7d594c864ddb18e4
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-07 01:15:43 -0300

    first commit

---
+++ desktop/src/net/felipebueno/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.felipebueno.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.felipebueno.flappybird.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
