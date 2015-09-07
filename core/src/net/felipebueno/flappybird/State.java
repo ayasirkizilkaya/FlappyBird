33c16f871f345ebdc3d6dc950f4ce22245c19456
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-07 01:55:47 -0300

    add assets

---
+++ core/src/net/felipebueno/flappybird/State.java
@@ -0,0 +1,7 @@
+package net.felipebueno.flappybird;
+
+/**
+ * Created by felipe on 07/09/15.
+ */
+public class State {
+}
---
+++ core/src/net/felipebueno/flappybird/states/GameStateManager.java
@@ -0,0 +1,7 @@
+package net.felipebueno.flappybird.states;
+
+/**
+ * Created by felipe on 07/09/15.
+ */
+public class GameStateManager {
+}
